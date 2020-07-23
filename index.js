eeb292db750cb4ac049a16652448908f5aacf03c
jlorenc1 <joshua.lorenc@icloud.com>
2020-07-23 10:08:40 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+const findMatching = (drivers, string) => {
+   return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase())
+}
+
+const fuzzyMatch = (drivers, string) => {
+    return drivers.filter(driver => driver.startsWith(string))
+}
+
+const matchName = (drivers, string) => {
+    return drivers.filter(driver => driver.name.toLowerCase() === string.toLowerCase())
+}
